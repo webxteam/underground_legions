934640b2fede7812495fe3e9646ed3732b7f88ac
Romulo Pransteter da Silva <r.pransteter@gmail.com>
2020-02-16 01:44:21 -0300

    Fixing last commit and implement new makefile commands...

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,10 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 const Express = require("express");
+const routes_1 = require("./core/middlewares/routes");
 const app = Express();
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(routes_1.default());
 app.listen(4001, () => {
-    console.log('It working on port 4001...');
+    console.log('It\'s working on port 4001...');
 });
 //# sourceMappingURL=app.js.map
